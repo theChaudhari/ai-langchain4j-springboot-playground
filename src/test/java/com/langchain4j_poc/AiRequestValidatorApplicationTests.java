95612af4b8d9aad8a463532fb72c0d7e28222058
Abhishek Chaudhari <abhichaudhari2297@gmail.com>
2026-03-03 17:43:32 +0530

    chore: rename artifactId to ai-task-agent & banner.txt

--- src/test/java/com/langchain4j_poc/AiRequestValidatorApplicationTests.java
+++ src/test/java/com/langchain4j_poc/AiRequestValidatorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Langchain4jPocApplicationTests {
+class AiRequestValidatorApplicationTests {
 
 	@Test
 	void contextLoads() {
