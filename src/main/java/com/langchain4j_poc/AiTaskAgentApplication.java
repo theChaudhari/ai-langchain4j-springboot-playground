201e8985537b358188d1033e1d0dd0b644309719
Abhishek Chaudhari <abhichaudhari2297@gmail.com>
2026-03-03 17:37:07 +0530

    chore: rename artifactId to ai-task-agent & banner.txt

--- src/main/java/com/langchain4j_poc/AiTaskAgentApplication.java
+++ src/main/java/com/langchain4j_poc/AiTaskAgentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Langchain4jPocApplication {
+public class AiTaskAgentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Langchain4jPocApplication.class, args);
+		SpringApplication.run(AiTaskAgentApplication.class, args);
 	}
 
 }
